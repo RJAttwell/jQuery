6431bf311e06627ec3d63682e969974aa9a790f7
RJAttwell <richardjattwell@gmail.com>
2022-06-07 11:49:53 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+//MANIPULATING STYLES WITH JQUERY
 //Using jQuery to target the h1 and change the css colour property to red
 $("h1").css("color", "red");
 
 $("button");
 //replaces document.querySelectorAll("button");
+
+//Can find out the current value of a particular property by using:
+console.log($("h1").css("font-size"));
+//One property = getting the value, two properties = setting the value
+
+//Can add CSS from styles.css file via jQuery:
+$("h1").addClass("bigTitle");
+
+//Can also remove CSS with:
+$("h1").removeClass("bigTitle");
+
+//To add/remove multiple classes:
+$("h1").addClass("bigTitle margin50");
+
+//Can query if an element possesses a class:
+$("h1").hasClass("bigTitle"); //Will return a boolean 
